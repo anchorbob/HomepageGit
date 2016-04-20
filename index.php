b53080b3f5abd8cdcf7e33ea22f554e75e32276f
Bo Zhang <gcbo@umich.edu>
2016-04-19 21:16:06 -0500

    init in Github. last version as a Michigan student

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bo Zhang</title>
+	
+    <meta http-equiv="keywords" content="Bo Zhang,Homepage,Umich">
+    <meta http-equiv="description" content="this is my page">
+    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">    
+    <link rel="stylesheet" href="style.css" type="text/css"></link>
+  </head>
+  
+  <body>
+    
+
+<table id="guide">  <!--导航栏-->
+    <tr>
+           <br/>                                                 
+             <td><a href="index.html" class="on"><span>Home</span></a></td>                                                 
+             <td><a href="about.html"><span>About</span></a></td>           
+                                                                    
+             <td><a href="projects.html"><span>Projects</span></a></td>
+			                                                          
+             <td><a href="ta.html"><span>TA EXPERIENCE</span></a></td>
+
+			       <td><a href="video.html"><span>video</span></a></td>
+      
+    </tr> 	
+</table> 
+      
+
+<div id ="style5" >  <!--内容栏-->
+
+  <ul>
+		<table cellspacing=10 width = 1000px>
+			<tr >
+  			<td width = 200px> 
+          <font size =5 face="Palatino"> 
+            <li><b>Basic Information</b></li>
+           </font> 
+        </td>
+    </table>
+
+    <table cellspacing=10 width = 700px>
+      <tr height = "10">  <td rowspan = 7 align = "left"  width = 180px> <IMG src="./index/psu1.jpg" height=220></td>  <td> </td>  </tr>
+      
+      <tr>  <td align = "left" colspan =2 > <font size =4 face="Palatino">Bo Zhang</font> </td> </tr>
+      
+      <tr>   <td width = 50px>Tel: </td>  <td>(734) 546-5863</td></tr>
+
+      <tr>   <td width = 50px>Email:</td>  <td>gcbo@umich.edu</td>   </tr>
+
+      <tr>   <td width = 50px>Webpsite:</td>  <td><a href=http://www-personal.umich.edu/~gcbo>http://www-personal.umich.edu/~gcbo </a></td> </tr>
+
+      <tr>   <td width>CV:</td>  <td><a href=./index/Bo_Zhang_EE_Mas_CV.pdf>Click here to view (Last edited: Feb 13th 2015)</a></td> </tr>
+
+    </table>
+  </ul>
+
+  <ul>
+    <table cellspacing=10 width = 1000px>
+      <tr>  <td align = "left"  colspan =2> <font size =5 face="Palatino"><li><b>Education Background </b></li></font></td>   </tr>
+
+      <tr > <td width = 70px>M.S.</td>  <td  width = 400>Electrical Engineering, University of Michigan, Ann Arbor</td> <td align = "left">Sept. 2014 - Now </td> </tr>
+
+      <tr > <td width = 70px>B.Eng.</td>  <td>Electrical Engineering, Shanghai Jiao Tong University</td>   <td align = "left">Sept. 2010 - Aug. 2014 </td> </tr>
+    </table>
+
+  </ul>
+
+</div> 
+</br>
+<?php
+    echo "This is a test";
+?>
+  <div class="Copyright">Copyright &copy; Bo Zhang &nbsp;&nbsp;&#8212;&nbsp;&nbsp; Last edited on Feb 27 2015</div>   
+  </body>
+</html>
